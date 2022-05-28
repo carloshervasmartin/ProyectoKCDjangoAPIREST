793e1901e2a73dc2a39864e02d0e5fb045312733
carloshervasmartin <carlos.hervas@expleogroup.com>
2022-05-28 19:38:40 +0200

    Falta el Detalle

---
+++ entries/api/serializers.py
@@ -0,0 +1,29 @@
+
+from datetime import datetime
+from rest_framework import serializers
+from entries.models import EntryModel
+
+class EntrySerializer(serializers.Serializer):
+    id = serializers.ReadOnlyField()
+    concept = serializers.CharField()
+    amount = serializers.FloatField()
+    datetime = serializers.DateTimeField()
+    
+    def create(self, validated_data):
+        instance = EntryModel(
+            datetime = validated_data.get('datetime'),
+            concept = validated_data.get('concept'),
+            amount = validated_data.get('amount')
+        )
+        
+        instance.save()
+        return instance
+        
+    def update(self, instance, validated_data):
+        instance.concept = validated_data.get('concept', instance.concept)
+        instance.amount = validated_data.get('amount', instance.amount)
+        instance.datetime = validated_data.get('datetime', instance.datetime)
+        
+        instance.save()
+        
+        return instance
